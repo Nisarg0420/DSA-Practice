abe9f32a5fe4ae92b929121f94bd32359b455751
Nisarg0420 <nisarg4806@gmail.com>
2026-06-23 19:29:47 +0530

    RECURSION QUESTION PRACTISE

--- Recursion/sum_of_digits.cpp
+++ Recursion/sum_of_digits.cpp
@@ -8,34 +8,33 @@ Output: 10  (1+2+3+4=10) */
 #include<iostream>
 using namespace std;
 
-void f(int arr[],int i,int sum)
+void f(int arr[],int i,int sum,int n)
 {
-   
-    if(i>3)
+    if(i>=n)
     {
-        cout<<sum<<endl;
+        cout<<"The Sum is: "<<sum<<endl;
         return;
     }
 
-    f(arr,i+1,sum+arr[i]);
+    f(arr,i+1,sum+arr[i],n);
 
 }
 
 int main()
 {
-    int a[4];
+    int n;
+    int a[20];
+
+    cout<<"Enter the Number of Elements: ";
+    cin>>n;
 
     cout<<"Enter the Number: ";
-    for(int i=0;i<4;i++)
+    for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
 
-    f(a,0,0);
-    
-    for(int i=0;i<4;i++)
-    {
-        cout<<a[i]<<endl;
-    }
+    f(a,0,0,n);
+
     return 0;
 }
